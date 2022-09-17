e0ec7197837e9af3696ac87c82f6403cb7245769
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2022-09-18 05:03:33 +0530

    changes

---
+++ Scraper.py
@@ -0,0 +1,18 @@
+from itertools import islice
+from youtube_comment_downloader import *
+import spacy
+downloader = YoutubeCommentDownloader()
+text=''
+comments = downloader.get_comments_from_url('https://www.youtube.com/watch?v=Kr2gj93wT0c', sort_by=SORT_BY_POPULAR)
+for comment in islice(comments, 23):
+    text+=comment
+
+nlp= spacy.load("en_core_web_sm")
+doc= nlp(text)
+token_list = [token for token in doc]
+filtered_tokens = [token for token in doc if not token.is_stop]
+lemmas = [
+     f"Token: {token}, lemma: {token.lemma_}"   
+     for token in filtered_tokens
+     ]
+filtered_tokens[1].vector
