978a6e6d5e5aa9a074b8f2adbe640233c8527be2
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2022-09-23 10:00:34 +0530

    changes

--- Scraper.py
+++ Scraper.py
@@ -5,7 +5,7 @@ downloader = YoutubeCommentDownloader()
 text=''
 comments = downloader.get_comments_from_url('https://www.youtube.com/watch?v=Kr2gj93wT0c', sort_by=SORT_BY_POPULAR)
 for comment in islice(comments, 23):
-    text+=comment
+    text+=comment['text']
 
 nlp= spacy.load("en_core_web_sm")
 doc= nlp(text)
@@ -15,4 +15,4 @@ lemmas = [
      f"Token: {token}, lemma: {token.lemma_}"   
      for token in filtered_tokens
      ]
-filtered_tokens[1].vector
+print(filtered_tokens[1].vector)
